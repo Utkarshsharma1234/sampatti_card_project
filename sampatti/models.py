e5e61d6c8f1864fc9c0e5b307ccea34332163af7
Ombalpelwad <balpelwadom@gmail.com>
2024-12-22 20:12:52 +0530

    LLM template updated

--- sampatti/models.py
+++ sampatti/models.py
@@ -82,10 +82,13 @@ class CashAdvanceManagement(Base):
     employerNumber = Column(Integer)
     worker_id = Column(Integer, default=0)
     employer_id = Column(String)
+    curentCashAdvance = Column(Integer, default= 0)
     cashAdvance = Column(Integer, default = 0)
     monthlyRepayment = Column(Integer, default=0)
     repaymentStartMonth = Column(String)
     repaymentStartYear = Column(Integer)
+    bonus = Column(Integer, default= 0)
+    Attendance = Column(Integer, default= 0)
 
 class CashAdvanceRecords(Base):
     __tablename__ = "CashAdvanceRecords"
