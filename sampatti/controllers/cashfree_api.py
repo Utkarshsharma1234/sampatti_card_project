7ff44a87651ae6a2e49e896b066496320e43ffff
Utkarshsharma1234 <96917045+Utkarshsharma1234@users.noreply.github.com>
2024-12-31 22:01:59 +0530

    payment link updated

--- sampatti/controllers/cashfree_api.py
+++ sampatti/controllers/cashfree_api.py
@@ -258,7 +258,8 @@ def payment_link_generation(db : Session):
         "January": 1, "February": 2, "March": 3, "April": 4,
         "May": 5, "June": 6,
         "July": 7, "August": 8, "September": 9,
-        "October": 10, "November": 11, "December": 12
+        "October": 10, "November": 11, "December": 12,
+        "sampatti" : 20
     }
 
     payment_ids = []
