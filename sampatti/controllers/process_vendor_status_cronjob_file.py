a419c5c908e5fc0d98df6af43e26d558562aebcd
Utkarshsharma1234 <96917045+Utkarshsharma1234@users.noreply.github.com>
2025-05-20 23:58:02 +0530

    cronjob file added for worker onboarding

--- sampatti/controllers/process_vendor_status_cronjob_file.py
+++ sampatti/controllers/process_vendor_status_cronjob_file.py
@@ -3,7 +3,7 @@ import requests
 def main():
 
     url = "https://conv.sampatticards.com/user/process_vendor_status_from_sheet" 
-    response = requests.get(url)
+    response = requests.post(url)
     if response.status_code == 200:
         print("Request successful:", response.json())
     else:
