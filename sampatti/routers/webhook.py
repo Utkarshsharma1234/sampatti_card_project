8a47d0676a30e266640609be17bb0a4419e1babf
Utkarshsharma1234 <96917045+Utkarshsharma1234@users.noreply.github.com>
2025-06-23 15:38:49 +0530

    commit

--- sampatti/routers/webhook.py
+++ sampatti/routers/webhook.py
@@ -84,20 +84,20 @@ async def orai_webhook(request: Request, db : Session = Depends(get_db)):
         print(f"Employernumber: {employerNumber}")
         print(f"Media Id: {media_id}")
 
-        # if not message_type:
-        #     pass
+        if not message_type:
+            pass
 
-        # elif message_type == "text":
-        #     body = message.get("text", {}).get("body")
-        #     userControllers.send_audio_message(body, "en-IN", employerNumber)
-        #     # whatsapp_message.send_greetings(employerNumber, template_name="salary_adjust_greetings")
-        #     # ai_agents.queryExecutor(employerNumber, message_type, body, "")
+        elif message_type == "text":
+            body = message.get("text", {}).get("body")
+            userControllers.send_audio_message(body, "en-IN", employerNumber)
+            # whatsapp_message.send_greetings(employerNumber, template_name="salary_adjust_greetings")
+            # ai_agents.queryExecutor(employerNumber, message_type, body, "")
         
-        # else:
-        #     media_id = message.get(message_type, {}).get("id")
-        #     # whatsapp_message.send_greetings(employerNumber, template_name="salary_adjust_greetings")
-        #     userControllers.send_audio_message("Hi this is testing audio which is created for testing the webhook.", "en-IN", employerNumber)
-        #     # ai_agents.queryExecutor(employerNumber, message_type, "", media_id)
+        else:
+            media_id = message.get(message_type, {}).get("id")
+            # whatsapp_message.send_greetings(employerNumber, template_name="salary_adjust_greetings")
+            userControllers.send_audio_message("Hi this is testing audio which is created for testing the webhook.", "en-IN", employerNumber)
+            # ai_agents.queryExecutor(employerNumber, message_type, "", media_id)
 
 
     except Exception as e:
