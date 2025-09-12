56e73fbffcc06ea1021caf31b062f6e3a13bd2bc
Utkarshsharma1234 <96917045+Utkarshsharma1234@users.noreply.github.com>
2025-09-13 01:34:24 +0530

    referral code logic added

--- sampatti/controllers/userControllers.py
+++ sampatti/controllers/userControllers.py
@@ -1585,7 +1585,7 @@ def generate_and_send_referral_code_to_employers(db : Session) :
 
     for employer in total_employers:
 
-        if employer.employerNumber != 916378639230:
+        if employer.employerNumber != 917665292549:
             continue
         
         if employer.FirstPaymentDone:
