45537054fce11308a6ed6cbc395263bdde523224
Ombalpelwad <balpelwadom@gmail.com>
2025-09-13 14:16:39 +0530

    post method

--- sampatti/controllers/referral_code_cronjob_file.py
+++ sampatti/controllers/referral_code_cronjob_file.py
@@ -2,7 +2,7 @@ import requests
 
 def main():
     url = "https://conv.sampatticards.com/user/send_referral_code" 
-    response = requests.get(url)
+    response = requests.post(url)
     if response.status_code == 200:
         print("Request successful:", response.json())
     else:
