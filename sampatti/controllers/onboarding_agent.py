68c19b7c8a7dffb18a603e418def9ef80870f98b
Ombalpelwad <balpelwadom@gmail.com>
2025-10-03 00:42:35 +0530

    gpt-4o

--- sampatti/controllers/onboarding_agent.py
+++ sampatti/controllers/onboarding_agent.py
@@ -23,7 +23,7 @@ load_dotenv()
 openai_api_key = os.environ.get("OPENAI_API_KEY")
 openrouter_api_key = os.environ.get("OPENROUTER_API_KEY")
 llm = ChatOpenAI(
-        model="openai/gpt-5", 
+        model="openai/gpt-4o", 
         api_key=openrouter_api_key,
         base_url="https://openrouter.ai/api/v1"
 )
