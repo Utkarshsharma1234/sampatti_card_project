08e0d333f2fe24dccf1a6aa76094e8bbb550eb0c
Utkarshsharma1234 <96917045+Utkarshsharma1234@users.noreply.github.com>
2025-10-30 01:14:53 +0530

    payment link authenticated

--- sampatti/controllers/cashfree_api.py
+++ sampatti/controllers/cashfree_api.py
@@ -263,8 +263,7 @@ def payment_link_generation(db : Session):
         "January": 1, "February": 2, "March": 3, "April": 4,
         "May": 5, "June": 6,
         "July": 7, "August": 8, "September": 9,
-        "October": 10, "November": 11, "December": 12,
-        "sampatti" : 20
+        "October": 10, "November": 11, "December": 12
     }
 
     payment_ids = []
