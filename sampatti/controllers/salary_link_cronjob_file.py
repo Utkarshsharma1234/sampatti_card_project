0ee35279a3bd4e6cf190de96667ff8cec0fb042c
Utkarshsharma1234 <96917045+Utkarshsharma1234@users.noreply.github.com>
2025-10-30 00:56:41 +0530

    cron testing

--- sampatti/controllers/salary_link_cronjob_file.py
+++ sampatti/controllers/salary_link_cronjob_file.py
@@ -1,9 +1,9 @@
 import requests
-# from sampatti.routers.auth import get_auth_headers
+from sampatti.routers.auth import get_auth_headers
 
 def main():
     url = "https://conv.sampatticards.com/cashfree/payment_link" 
-    response = requests.get(url)
+    response = requests.get(url, headers=get_auth_headers())
     if response.status_code == 200:
         print("Request successful:", response.json())
     else:
